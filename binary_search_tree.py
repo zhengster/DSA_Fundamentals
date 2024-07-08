c348ef8b1cadbd2a75c7695793f4808a68dcaeb0
zhengster <azheng371@gmail.com>
2024-07-08 16:49:39 -0400

    Create binary_search_tree.py

---
+++ binary_search_tree.py
@@ -0,0 +1,23 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+    def __eq__(self, other):
+        pass          
+
+# DFS 
+
+# BFS
+
+# PRE-ORDER
+
+# IN-ORDER
+
+# POST-ORDER
+
+# BALANCED BINARY TREE
+
+# LOWEST COMMON ANCESTOR 
+
